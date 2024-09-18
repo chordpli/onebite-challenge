f2690c647ba9e8fcc3c480c9f8ac1b5554ba4162
pli <chordplaylist@gmail.com>
2024-09-18 18:45:38 +0900

    feat: mission day5

--- onebite-challenge-cinema/src/pages/index.tsx
+++ onebite-challenge-cinema/src/pages/index.tsx
@@ -1,10 +1,28 @@
 import SearchBar from "@/components/Search-bar";
+import MovieItem from "@/components/Movie-item";
+import movies from "@/dummy.json";
+import style from './index.module.css';
 
 export default function Home() {
     return (
-        <div>
+        <div className={style.container}>
             <SearchBar/>
-            <h2>ONEBITE CINEMA</h2>
+            <div>
+                <div>지금 가장 추천하는 영화</div>
+                <div className={style.reco_container}>
+                    {movies.slice(0, 3).map((movie) => (
+                        <MovieItem key={`recomovie-${movie.id}`} {...movie} />
+                    ))}
+                </div>
+            </div>
+            <div>
+                <div>등록된 모든 영화</div>
+                <div className={style.all_container}>
+                    {movies.map((movie) => (
+                        <MovieItem key={`recomovie-${movie.id}`} {...movie} />
+                    ))}
+                </div>
+            </div>
         </div>
     );
 }
--- onebite-challenge-cinema/src/pages/search/index.tsx
+++ onebite-challenge-cinema/src/pages/search/index.tsx
@@ -1,18 +1,27 @@
 import {useRouter} from "next/router";
-import {ReactNode} from "react";
 import SearchBar from "@/components/Search-bar";
+import movies from "@/dummy.json";
+import MovieItem from "@/components/Movie-item";
+import {MovieData} from "@/types";
+import style from './index.module.css';
 
 export default function Page() {
+
     const router = useRouter();
     const {q} = router.query;
 
     return (
         <div>
-            <SearchBar />
+            <SearchBar/>
             {q && (
-                <h2>검색 결과 : {q}</h2>
-            )
-            }
+                <div className={style.search_container}>
+                    {movies.filter(
+                        (movie: MovieData) => movie.title.includes(q as string)
+                    ).map((movie) => (
+                        <MovieItem key={movie.id} {...movie} />
+                    ))}
+                </div>
+            )}
         </div>
     );
 }
